ae6bc538b3c87a4c5db6da8895116ab5ee0a12be
theladykiller <niki.plachetzky@gmail.com>
2024-05-13 16:34:55 +0200

    function fulfills required ouput for new test case

--- string_calculator/spec/help_alf_spec.js
+++ string_calculator/spec/help_alf_spec.js
@@ -1,11 +1,16 @@
 describe('String Calculator', function() {
 	beforeEach(function() {
-		help_alf = new StringCalculator();
+		help_alf = new help_alf_function();
 	});
 
 	it('should return "Spaceship lost forever" if ship could not be found', function() {
 		let map = "..........\n..........\n..........\n..........\n..........\n..........";
 		expect(help_alf.find_ship(map)).toEqual("Spaceship lost forever");
+	});
+
+	it('should return "0,0" if ship is at coordinates: 0,0', function() {
+		let map = "..........\n..........\n..........\n..........\n..........\nX.........";
+		expect(help_alf.find_ship(map)).toEqual("0,0");
 	});
 
 });
--- string_calculator/src/help_alf.js
+++ string_calculator/src/help_alf.js
@@ -1,5 +1,25 @@
-// StringCalculator.js
-function StringCalculator() {}
-StringCalculator.prototype.find_ship = function(map) {
-	return "Spaceship lost forever"
+function help_alf_function() {}
+help_alf_function.prototype.find_ship = function(map) {
+	let lines = map.split("\n")
+	
+	let fixed_x = null
+	let fixed_y = null
+	let position_y = 5
+	for (let line of lines){
+		let position_x = 0
+		for (let char of line){
+			if (char == "X"){
+				fixed_x = position_x
+				fixed_y = position_y
+			}
+			position_x++
+		}
+		position_y--
+	}
+
+	if(fixed_y == null || fixed_x == null)
+		return "Spaceship lost forever"
+	else
+		return fixed_x + "," + fixed_y
+
 };
