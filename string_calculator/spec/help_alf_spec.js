c08538a468a70e5dc73766da69654fc523448a70
theladykiller <niki.plachetzky@gmail.com>
2024-05-13 17:02:36 +0200

    added new test case

--- string_calculator/spec/help_alf_spec.js
+++ string_calculator/spec/help_alf_spec.js
@@ -1,20 +1,27 @@
 describe('String Calculator', function() {
 	beforeEach(function() {
-		help_alf = new help_alf_function();
+		help_alf = new help_alf_function()
 	});
 
 	it('should return "Spaceship lost forever" if ship could not be found', function() {
-		let map = "..........\n..........\n..........\n..........\n..........\n..........";
+		let map = "..........\n..........\n..........\n..........\n..........\n.........."
 		expect(help_alf.find_ship(map)).toEqual("Spaceship lost forever");
 	});
 
 	it('should return "0,0" if ship is at coordinates: 0,0', function() {
-		let map = "..........\n..........\n..........\n..........\n..........\nX.........";
+		let map = "..........\n..........\n..........\n..........\n..........\nX........."
 		expect(help_alf.find_ship(map)).toEqual("0,0");
 	});
 
 	it('should return "4,3" if ship is at coordinates: 4,3', function() {
-		let map = "..........\n..........\n....X.....\n..........\n..........\n..........";
+		let map = "..........\n..........\n....X.....\n..........\n..........\n.........."
 		expect(help_alf.find_ship(map)).toEqual("4,3");
 	});
+
+	it('should return "7,3" if ship is at coordinates: 7,3', function() {
+		let map = "..........\n..........\n..........\n..........\n..........\n..........\n.......X..\n..........\n..........\n.........."
+		expect(help_alf.find_ship(map)).toEqual("7,3");
+	});
+
+	
 });
